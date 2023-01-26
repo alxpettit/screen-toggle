6fc81c37ff59407d01fe0733c1a0aac99ea10332
Alexandria <alxpettit@gmail.com>
2023-01-25 23:49:32 -0700

    passive-aggressive comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// So every time I locked one of my machines, lately, the screen would turn off like it's supposed to,
+// but then it'd turn right back on. I couldn't find any information on why such a thing would happen,
+// but I strongly suspect it's because GNOME3 is utter garbage.
+
+// Anyway, this program just catches the right key combo and then calls `xset` to "correct" the issue.
+// Of course, it would turn it back on immediately if it only called xset once to turn the screen off,
+// so instead it runs it over and over and over and over and over and over and over and...
+
+// This is horrible
+// Do not learn from me, kids
+// I am not a good role model.
+
 use rdev::EventType::{KeyPress, KeyRelease};
 use rdev::Key::{Alt, AltGr, ControlLeft, ControlRight, Insert, KeyD, KeyE, KeyS, ShiftLeft};
 use rdev::{listen, Event, Key};
@@ -68,13 +80,20 @@ impl ScreenStateEnforcer {
         let update = thread::spawn(move || loop {
             match state_ptr.is_on() {
                 true => {
+                    // Debounce against previous state so we only send if state has changed
                     if state_ptr != old_state_ptr {
+                        // Send command to turn screen on
                         Self::send_on_cmd().expect("Could not send.");
                         old_state_ptr.set_from(&state_ptr);
                     }
                 }
                 false => {
                     while state_ptr.is_off() {
+                        // Now we turn off the screens over and over EVERY 100 MS
+                        // faster than whatever's turning them on can act
+                        // Yes, this is horrifying.
+                        // Yes, I don't care.
+                        // Fuck off.
                         Self::send_off_cmd().expect("Could not send.");
                         thread::sleep(Duration::from_millis(100));
                     }
@@ -89,6 +108,7 @@ impl ScreenStateEnforcer {
         }
     }
 
+    /// Send command to turn screen off
     fn send_off_cmd() -> Result<Output, Box<dyn Error>> {
         Ok(Command::new("xset")
             .arg("dpms")
@@ -98,6 +118,7 @@ impl ScreenStateEnforcer {
             .wait_with_output()?)
     }
 
+    /// Send command to turn screen on
     fn send_on_cmd() -> Result<Output, Box<dyn Error>> {
         Ok(Command::new("xset")
             .arg("dpms")
@@ -123,6 +144,9 @@ impl Clone for KeyStates {
     }
 }
 
+// BTW Wrapping Arc<Mutex<>> like this is probably a bad idea for any _real_ API
+// As it would probably be easy for the API-user to end up with deadlocks and be very confused ;)
+
 impl KeyStates {
     fn new() -> Self {
         let mut map = HashMap::new();
@@ -133,8 +157,8 @@ impl KeyStates {
         *self
             .0
             .lock()
-            .unwrap()
-            .entry(key)
+            .unwrap() // ah yes, 500 unwrap() in your codebase
+            .entry(key) // truly masterful programming quality
             .or_insert(KeyState::Released)
     }
 
@@ -182,6 +206,9 @@ fn main() {
     let mut ssenforcer = ScreenStateEnforcer::new();
     let mut time_since_last_toggle = Instant::now();
     loop {
+        // Disgusting but effective
+        // FYI the correct way to do this is via a callback
+        // so that it's not constantly wasting CPU
         match (
             kb.states.get_state(ControlLeft),
             kb.states.get_state(Alt),
